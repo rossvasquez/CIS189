36dd151b6ac8db65ce63831c617d949f9a7fc713
rossvasquez <60303452+rossvasquez@users.noreply.github.com>
2020-06-24 15:36:14 -0500

    Add files via upload

---
+++ set_membership.py
@@ -0,0 +1,11 @@
+def in_set():
+    a = set('12345')
+
+    b = input('Enter a number to see if it is the set: ')
+
+    if b in a:
+        print('True')
+    else:
+        print('False')
+
+in_set()
