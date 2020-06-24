d36647398d0479df6776dccaecd1e2820b9e0926
rossvasquez <60303452+rossvasquez@users.noreply.github.com>
2020-06-24 15:29:19 -0500

    Add files via upload

---
+++ test_set_membership.py
@@ -0,0 +1,12 @@
+def test():
+    a = set('12345')
+    if '5' in a:
+        print('True')
+    else:
+        print('False')
+    if '6' in a:
+        print('True')
+    else:
+        print('False')
+
+test()
